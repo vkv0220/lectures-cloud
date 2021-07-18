1a6d8f2da0059496bc8e4674e1f1ec130ca481d9
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2021-07-18 15:59:20 +0700

    Create hw-1.py
    added Python 1 task

---
+++ Python/hw-1.py
@@ -0,0 +1,30 @@
+
+
+def print_type(variable):
+    """This function prints the type of supplied variable
+
+    Args:
+        variable (Any): variable to print type for
+    """
+    # fill the gaps ...
+
+
+def do_action(variable):
+    """Does the action depending on variable's type. (hint: you can either use print function right here, 
+    or return the result. For now it doesn't matter)
+    Defined actions:
+        int: square the number
+        float: and π(pi) (from math.pi, don't forget the import~!) to it and print the result
+        bool: inverse it (e.g if you have True, make it False) and print the result
+        list: print elements in reversed order
+
+    Args:
+        variable (Any): variable to perform action on
+    """
+
+
+variables = [ 42, 45.0, True, False, [16, 9, 43, 65, 97, 0]]
+
+# Please, for all elements in `variables` list print the following:
+#  the type of variable using `print_type` function
+#  and the action for variable using `do_action` function
