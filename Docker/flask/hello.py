4c4b0efcbf96f0c446e369a4a18ada3e3ad9219e
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2021-08-19 17:58:58 +0700

    Updated hello

--- Docker/flask/hello.py
+++ Docker/flask/hello.py
@@ -17,27 +17,35 @@ print(res_text['main']['temp'])
 print(param)
 print(sys.argv[1])
 
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host="db",
+            database="app_db",
+            user="app_user",
+            password="app_pass")
+        cur = conn.cursor()
+        datalist = []
+        #cur.execute('SELECT version()')
+        cur.execute('CREATE TABLE IF NOT EXISTS apptable (id SERIAL PRIMARY KEY, value VARCHAR(255) NOT NULL)')
+        sql = f"INSERT INTO apptable (value) VALUES ( { res_text['main']['temp'] } )"
+        cur.execute(sql)
+
+        cur.execute('SELECT * FROM apptable')
+        rows = cur.fetchall()
+        for row in rows:
+            datalist.append(row)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return datalist
+
 @app.route('/')
 def hello():
-    conn = psycopg2.connect(
-    host="db",
-    database="app_db",
-    user="app_user",
-    password="app_pass")
-    cur = conn.cursor()
-    #cur.execute('SELECT version()')
-    cur.execute('CREATE TABLE IF NOT EXISTS apptable (id SERIAL PRIMARY KEY, value VARCHAR(255) NOT NULL)')
-    sql = f"INSERT INTO apptable (value) VALUES ( { res_text['main']['temp'] } )"
-    cur.execute(sql)
-
-    cur.execute('SELECT * FROM apptable')
-
-    datalist = []
-    rows = cur.fetchall()
-    for row in rows:
-        datalist.append(row)
-
-        cur.close()
-        conn.commit()
-
+    datalist = connect()
     return f'<div> Привет, { param } </div> <div> Погода сегодня. В: { res_text["name"] } температура воздуха: { res_text["main"]["temp"] } </div><p>{str(datalist)}</p>'
