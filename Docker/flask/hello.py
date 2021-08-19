a8bd48aaf9241d94fbc40e9d58c757056616eba9
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2021-08-19 17:29:26 +0700

    Flask with database

--- Docker/flask/hello.py
+++ Docker/flask/hello.py
@@ -9,11 +9,6 @@ param=os.environ.get('NAME')
 api_key=os.environ.get('API_KEY')
 
 request_string=f"https://api.openweathermap.org/data/2.5/weather?id=1489425&lang=ru&units=metric&appid={api_key}"
-# conn = psycopg2.connect(
-#             host=os.environ.get('DBHOST'),
-#             database="postgres",
-#             user="postgres",
-#             password="postgres")
 
 response = requests.get(request_string)
 res_text = json.loads(response.text)
@@ -21,10 +16,29 @@ print(res_text['name'])
 print(res_text['main']['temp'])
 print(param)
 print(sys.argv[1])
-# print('PostgreSQL database version:')
-# cur.execute('SELECT version()')
+
+conn = psycopg2.connect(
+            host="db",
+            database="app_db",
+            user="app_user",
+            password="app_pass")
+cur = conn.cursor()
+#cur.execute('SELECT version()')
+cur.execute('CREATE TABLE IF NOT EXISTS apptable (id SERIAL PRIMARY KEY, value VARCHAR(255) NOT NULL)')
+sql = f"INSERT INTO apptable (value) VALUES ( { res_text['main']['temp'] } )"
+cur.execute(sql)
+
+cur.execute('SELECT * FROM apptable')
+
+datalist = []
+rows = cur.fetchall()
+for row in rows:
+    datalist.append(row)
+
+cur.close()
+conn.commit()
 
 @app.route('/')
 def hello():
     print(str(response))
-    return f'<div> Привет, { param } </div> <div> Погода сегодня. В: { res_text["name"] } температура воздуха: { res_text["main"]["temp"] } </div>'
+    return f'<div> Привет, { param } </div> <div> Погода сегодня. В: { res_text["name"] } температура воздуха: { res_text["main"]["temp"] } </div><p>{str(datalist)}</p>'
